197f48d4442d92615db499aea5807aa81d1bc364
Clarence Kwong <clarence.kwong@pcbank.ca>
2023-02-09 17:01:12 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `prop-type-head-example`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
